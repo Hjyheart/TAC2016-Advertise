1254250e1b0677e96a1a870c39dae93fbee949f0
LRui <756257081@qq.com>
2016-05-26 21:21:50 +0800

    Version 1.0

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ $(document).ready(function() {
         //Navigation
         menu: '#menu',
         lockAnchors: false,
-        anchors:['first', 'second', 'third', 'forth', 'fifth', 'sixth'],
-        navigation: false,
+        anchors:['first', 'second', 'third', 'forth', 'fifth', 'sixth', 'seventh'],
+        navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide', 'forthSlide', 'fifthSlide', 'sixthSlide'],
+        navigationTooltips: ['', '', '', '', '', '', ''],
         showActiveTooltip: false,
         slidesNavigation: true,
         slidesNavPosition: 'bottom',
@@ -60,63 +60,48 @@ $(document).ready(function() {
                 $('#one img').stop(true, true).fadeIn(2500);
                 $('#one .oneText').stop(true, true).delay(1000).fadeIn(2500);
             } else if (index == 2) {
-                $('#two .code').stop(true, true).animate({height: '240px'}, 3000, function () {
-                    $('#two .picContainer').stop(true, true).animate({height: '220px'}, 2000);
-                });
-            } else if (index == 3) {
-                $('#three img:even').stop(true, true).animate({left: '0'}, 1000);
-                $('#three img:odd').stop(true, true).animate({right: '0'}, 1000);
+                $('#two img:even').stop(true, true).animate({left: '0'}, 1000);
+                $('#two img:odd').stop(true, true).animate({right: '0'}, 1000);
             } else if (index == 4) {
+                $('#four .code').stop(true, true).animate({height: '240px'}, 3000, function () {
+                    $('#four .picContainer').stop(true, true).animate({height: '220px'}, 2000);
+                });
+            } else if (index == 5) {
                 var delayArray = [3.5, 3, 1.5, 1, 2.5, 2, 0.5, 4, 0];
-                $('#four img').each(function (index) {
+                $('#five img').each(function (index) {
                     $(this).css('transition', '');
                     $(this).removeClass('drop').addClass('up');
                     $(this).css('transition', 'transform 1.5s ease ' + delayArray[index] + 's');
                     $(this).removeClass('up').addClass('drop');
                 });
-                $('#four h3:last').stop(true, true).delay(6000).fadeIn(1000);
+                $('#five h3:last').stop(true, true).delay(6000).fadeIn(1000);
+            } else if (index == 7) {
+                $('#seven h1').stop(true, true).fadeIn(2500);
+                $('#seven h2').stop(true, true).delay(1000).fadeIn(2500);
             }
         },
 
         onLeave: function(index, nextIndex, direction) {
-            if (index == 1) {
-                $('#up').show();
-            }
-            if (index == 6) {
-                $('#down').show();
-            }
-
-            if (index == 2 && direction == 'up') {
-                $('#up').hide();
-            }
-            if (index == 5 && direction == 'down') {
-                $('#down').hide();
-            }
 
             if (index == 1) {
                 $('#one img').stop(true, true).hide();
                 $('#one .oneText').stop(true, true).hide();
             } else if (index == 2) {
-                $('#two .code').stop(true, true).height(0);
-                $('#two .picContainer').stop(true, true).height(0);
-            } else if (index == 3) {
-                $('#three img:even').stop(true, true).css({'left': '-100%'});
-                $('#three img:odd').stop(true, true).css({'right': '-100%'});
+                $('#two img:even').stop(true, true).css({'left': '-100%'});
+                $('#two img:odd').stop(true, true).css({'right': '-100%'});
             } else if (index == 4) {
-                $('#four img').each(function () {
+                $('#four .code').stop(true, true).height(0);
+                $('#four .picContainer').stop(true, true).height(0);
+            } else if (index == 5) {
+                $('#five img').each(function () {
                     $(this).css('transition', '');
                     $(this).removeClass('drop').addClass('up');
                 });
-                $('#four h3:last').stop(true, true).fadeOut();
+                $('#five h3:last').stop(true, true).fadeOut(0);
+            } else if (index == 7) {
+                $('#seven h1').stop(true, true).hide();
+                $('#seven h2').stop(true, true).hide();
             }
         }
-    });
-
-    $('#up').click(function () {
-        $.fn.fullpage.moveSectionUp();
-    });
-
-    $('#down').click(function () {
-        $.fn.fullpage.moveSectionDown();
     });
 });
