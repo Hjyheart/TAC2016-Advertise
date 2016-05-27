a4aefc990aa1f2ddfd39bd3cfd7eefe0f5994790
LRui <756257081@qq.com>
2016-05-27 16:17:38 +0800

    Fix many bug and fix some screens

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         //Navigation
         menu: '#menu',
         lockAnchors: false,
-        anchors:['first', 'second', 'third', 'forth', 'fifth', 'sixth', 'seventh'],
+        anchors:['one', 'two', 'three', 'four', 'five', 'six', 'seven'],
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['', '', '', '', '', '', ''],
@@ -37,7 +37,7 @@ $(document).ready(function() {
         //Accessibility
         keyboardScrolling: true,
         animateAnchor: true,
-        recordHistory: true,
+        recordHistory: false,
 
         //Design
         controlArrows: true,
@@ -57,50 +57,49 @@ $(document).ready(function() {
         afterLoad: function(anchorLink, index) {
 
             if (index == 1) {
-                $('#one img').stop(true, true).fadeIn(2500);
-                $('#one .oneText').stop(true, true).delay(1000).fadeIn(2500);
+                $('#tac2016 img').stop(true, true).fadeIn(2500);
+                $('#tac2016 .oneText').stop(true, true).delay(1000).fadeIn(2500);
             } else if (index == 2) {
-                $('#two img:even').stop(true, true).animate({left: '0'}, 1000);
-                $('#two img:odd').stop(true, true).animate({right: '0'}, 1000);
-            } else if (index == 4) {
-                $('#four .code').stop(true, true).animate({height: '240px'}, 3000, function () {
-                    $('#four .picContainer').stop(true, true).animate({height: '220px'}, 2000);
+                $('#tac img').stop(true, true).animate({left: '0'}, 1000);
+            } else if (index == 3) {
+                $('#workshop .code').stop(true, true).animate({height: '240px'}, 3000, function () {
+                    $('#workshop .picContainer').stop(true, true).animate({height: '400px'}, 2000);
                 });
-            } else if (index == 5) {
+            } else if (index == 4) {
                 var delayArray = [3.5, 3, 1.5, 1, 2.5, 2, 0.5, 4, 0];
-                $('#five img').each(function (index) {
+                $('#technology img').each(function (index) {
                     $(this).css('transition', '');
                     $(this).removeClass('drop').addClass('up');
                     $(this).css('transition', 'transform 1.5s ease ' + delayArray[index] + 's');
                     $(this).removeClass('up').addClass('drop');
                 });
-                $('#five h3:last').stop(true, true).delay(6000).fadeIn(1000);
+                $('#technology h3:last').stop(true, true).delay(6000).fadeIn(1000);
             } else if (index == 7) {
-                $('#seven h1').stop(true, true).fadeIn(2500);
-                $('#seven h2').stop(true, true).delay(1000).fadeIn(2500);
+                $('#welcome h1').stop(true, true).fadeIn(2500);
+                $('#welcome h2').stop(true, true).delay(1000).fadeIn(2500);
             }
         },
 
         onLeave: function(index, nextIndex, direction) {
 
             if (index == 1) {
-                $('#one img').stop(true, true).hide();
-                $('#one .oneText').stop(true, true).hide();
+                $('#tac2016 img').stop(true, true).hide();
+                $('#tac2016 .oneText').stop(true, true).hide();
             } else if (index == 2) {
-                $('#two img:even').stop(true, true).css({'left': '-100%'});
-                $('#two img:odd').stop(true, true).css({'right': '-100%'});
+                $('#tac img:even').stop(true, true).css({'left': '1500px'});
+                $('#tac img:odd').stop(true, true).css({'left': '-1500px'});
+            } else if (index == 3) {
+                $('#workshop .code').stop(true, true).height(0);
+                $('#workshop .picContainer').stop(true, true).height(0);
             } else if (index == 4) {
-                $('#four .code').stop(true, true).height(0);
-                $('#four .picContainer').stop(true, true).height(0);
-            } else if (index == 5) {
-                $('#five img').each(function () {
+                $('#technology img').each(function () {
                     $(this).css('transition', '');
                     $(this).removeClass('drop').addClass('up');
                 });
-                $('#five h3:last').stop(true, true).fadeOut(0);
+                $('#technology h3:last').stop(true, true).fadeOut(0);
             } else if (index == 7) {
-                $('#seven h1').stop(true, true).hide();
-                $('#seven h2').stop(true, true).hide();
+                $('#welcome h1').stop(true, true).hide();
+                $('#welcome h2').stop(true, true).hide();
             }
         }
     });
